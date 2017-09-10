9a4ac9d154edd3dbb66fd0e771ffaef98ef19640
vcordero07 <vcordero07@gmail.com>
2017-09-09 23:42:49 -0400

    wip-added get test endpoint

--- test/test-blogapp.js
+++ test/test-blogapp.js
@@ -64,6 +64,51 @@ describe('Blog App API resource', function() {
     return closeServer();
   })
 
+  describe('GET endpoint', function() {
+
+    it('should return all existing posts', function() {
+
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          //res.body.posts.should.have.length.of.at.least(1);
+          return BlogPost.count();
+        })
+        .then(count => {
+          //res.body.should.have.length.of(count);
+        });
+    });
+
+    it('should return posts with right fields', function() {
+
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(res => {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.length.of.at.least(1);
+
+          res.body.forEach(post => {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'title', 'content', 'author', 'created');
+          });
+
+          resPost = res.body[0];
+          return BlogPost.findById(resPost.id);
+        })
+        .then(post => {
+          resPost.title.should.equal(post.title);
+          resPost.content.should.equal(post.content);
+          resPost.author.should.equal(post.authorName);
+        });
+    });
+  });
+
   describe('POST endpoint', function() {
 
     it('should add a new blog post', function() {
